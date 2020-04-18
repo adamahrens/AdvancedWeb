a7b8f984d4f971b51cbf32dab594c0634834217d
Adam Ahrens <adam.aahrens@gmail.com>
2020-04-17 21:39:10 -0500

    Adding random box color with state

--- ReactState/random-box/src/App.js
+++ ReactState/random-box/src/App.js
@@ -1,26 +1,66 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import Box from './Box'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const NUMBER_OF_BOXES = 32;
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    const boxes = Array(NUMBER_OF_BOXES).fill().map(this.randomColor, this);
+    this.state = { boxes }
+
+    setInterval(() => {
+      const previousBoxes = this.state.boxes.slice();
+      const randomxIdx = Math.floor(Math.random() * previousBoxes.length);
+      previousBoxes[randomxIdx] = this.randomColor();
+      this.setState({ boxes: previousBoxes }, () => {
+        console.log('State has been updated');
+      });
+    }, 300);
+  }
+
+  randomColor() {
+    let idx = Math.floor(Math.random() * this.props.allColors.length);
+    let colorName = this.props.allColors[idx];
+    console.log(`Random Color is ${colorName}`);
+    return colorName;
+  }
+
+  render() {
+    const boxes = this.state.boxes.map((boxColor, index) => {
+      return <Box key={index} color={boxColor} />
+    });
+
+    return (
+      <div className="App">
+        {boxes}
+      </div>
+    );
+  }
 }
+
+App.defaultProps = {
+  allColors: ["AliceBlue", "AntiqueWhite", "Aqua", "Aquamarine", "Azure", "Beige", "Bisque", "Black", "BlanchedAlmond",
+    "Blue", "BlueViolet", "Brown", "BurlyWood", "CadetBlue", "Chartreuse", "Chocolate",
+    "Coral", "CornflowerBlue", "Cornsilk", "Crimson", "Cyan", "DarkBlue", "DarkCyan", "DarkGoldenRod",
+    "DarkGray", "DarkGrey", "DarkGreen", "DarkKhaki", "DarkMagenta", "DarkOliveGreen", "Darkorange",
+    "DarkOrchid", "DarkRed", "DarkSalmon", "DarkSeaGreen", "DarkSlateBlue", "DarkSlateGray", "DarkSlateGrey",
+    "DarkTurquoise", "DarkViolet", "DeepPink", "DeepSkyBlue", "DimGray", "DimGrey", "DodgerBlue",
+    "FireBrick", "FloralWhite", "ForestGreen", "Fuchsia", "Gainsboro", "GhostWhite", "Gold", "GoldenRod",
+    "Gray", "Grey", "Green", "GreenYellow", "HoneyDew", "HotPink", "IndianRed", "Indigo", "Ivory", "Khaki",
+    "Lavender", "LavenderBlush", "LawnGreen", "LemonChiffon", "LightBlue", "LightCoral", "LightCyan",
+    "LightGoldenRodYellow", "LightGray", "LightGrey", "LightGreen", "LightPink", "LightSalmon",
+    "LightSeaGreen", "LightSkyBlue", "LightSlateGray", "LightSlateGrey", "LightSteelBlue", "LightYellow",
+    "Lime", "LimeGreen", "Linen", "Magenta", "Maroon", "MediumAquaMarine", "MediumBlue", "MediumOrchid",
+    "MediumPurple", "MediumSeaGreen", "MediumSlateBlue", "MediumSpringGreen", "MediumTurquoise",
+    "MediumVioletRed", "MidnightBlue", "MintCream", "MistyRose", "Moccasin", "NavajoWhite", "Navy",
+    "OldLace", "Olive", "OliveDrab", "Orange", "OrangeRed", "Orchid", "PaleGoldenRod", "PaleGreen",
+    "PaleTurquoise", "PaleVioletRed", "PapayaWhip", "PeachPuff", "Peru", "Pink", "Plum", "PowderBlue",
+    "Purple", "Red", "RosyBrown", "RoyalBlue", "SaddleBrown", "Salmon", "SandyBrown", "SeaGreen",
+    "SeaShell", "Sienna", "Silver", "SkyBlue", "SlateBlue", "SlateGray", "SlateGrey", "Snow", "SpringGreen",
+    "SteelBlue", "Tan", "Teal", "Thistle", "Tomato", "Turquoise", "Violet", "Wheat", "White", "WhiteSmoke",
+    "Yellow", "YellowGreen"]
+};
 
 export default App;
---
+++ ReactState/random-box/src/Box.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Box = (props) => {
+    const style = {
+        width: '180px',
+        height: '180px',
+        display: 'inline-block',
+        backgroundColor: props.color
+    }
+
+    return <div style={style} />;
+};
+
+export default Box;
