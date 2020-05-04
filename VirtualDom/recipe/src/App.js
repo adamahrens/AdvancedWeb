e6437ec179ea70c53d5ff5f60952890566e77d53
Adam Ahrens <adam.aahrens@gmail.com>
2020-05-04 16:04:30 -0500

    Adding state to recipe app

--- VirtualDom/recipe/src/App.js
+++ VirtualDom/recipe/src/App.js
@@ -1,26 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import RecipeList from './RecipeList'
+import Navbar from './Navbar'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      recipes: [
+        {
+          id: 1,
+          title: "Monster Cookie",
+          ingredients: ['Sugar', 'Peanut Butter', 'Brown Sugar', 'Oats'],
+          instructions: "Add all the ingredients and mix",
+          image: "https://images.unsplash.com/photo-1559622214-f8a9850965bb?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=3101&q=80"
+        },
+        {
+          id: 2,
+          title: "Monster Cookie",
+          ingredients: ['Sugar', 'Peanut Butter', 'Brown Sugar', 'Oats'],
+          instructions: "Add all the ingredients and mix",
+          image: "https://images.unsplash.com/photo-1559622214-f8a9850965bb?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=3101&q=80"
+        },
+        {
+          id: 3,
+          title: "Monster Cookie",
+          ingredients: ['Sugar', 'Peanut Butter', 'Brown Sugar', 'Oats'],
+          instructions: "Add all the ingredients and mix",
+          image: "https://images.unsplash.com/photo-1559622214-f8a9850965bb?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=3101&q=80"
+        }
+      ],
+      nextRecipeId: 4
+    }
+  }
+
+  render() {
+    return (
+      <div className="App" >
+        <Navbar />
+        <RecipeList recipes={this.state.recipes} />
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ VirtualDom/recipe/src/Recipe.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import './Recipe.css'
+
+class Recipe extends Component {
+    static propTypes = {
+        id: PropTypes.number.isRequired,
+        title: PropTypes.string.isRequired,
+        ingredients: PropTypes.arrayOf(PropTypes.string).isRequired,
+        instructions: PropTypes.string.isRequired,
+        img: PropTypes.string.isRequired,
+    }
+
+    render() {
+        const { id, title, image, instructions } = this.props;
+        const ingredients = this.props.ingredients.map((ingredient, index) => (
+            <li key={index}>{ingredient}</li>
+        ))
+
+        return (
+            <div className="recipe-card" data-id={id} >
+                <img src={image} alt={title} />
+                <div className="recipe-details">
+                    <h1>{title}</h1>
+                    <h4>Ingredients:</h4>
+                    <ul>
+                        {ingredients}
+                    </ul>
+                    <h5>Instructions:</h5>
+                    <p>{instructions}</p>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Recipe;
