309a4d15c6ce843b2a2a2ae7db239c08f28eb248
Adam Ahrens <adam.aahrens@gmail.com>
2020-05-04 11:23:04 -0500

    Updating state on submission of form

--- VirtualDom/todo/src/App.js
+++ VirtualDom/todo/src/App.js
@@ -1,14 +1,38 @@
-import React from 'react';
+import React, { Component } from 'react';
 import TodoList from './TodoList'
 import './App.css';
 
-function App() {
-  const todos = ["Walk The Dog", "Go to the Gym", "Read a Book"]
-  return (
-    <div className="App">
-      <TodoList todos={todos} />
-    </div>
-  );
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = { todos: ['Walk the Dog'], todoText: '' };
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <form onSubmit={(event) => {
+          event.preventDefault();
+          this.setState((prevState, props) => {
+            return { todos: [...prevState.todos, prevState.todoText], todoText: '' }
+          });
+        }}>
+          <label>
+            Todo:
+              <input
+              type="text"
+              name="todoText"
+              value={this.state.todoText}
+              onChange={(event) => { this.setState({ [event.target.name]: event.target.value }) }}
+            />
+          </label>
+          <input type="submit" value="Submit" />
+        </form>
+        <TodoList todos={this.state.todos} />
+      </div>
+    );
+  }
 }
 
 export default App;
--- VirtualDom/todo/src/TodoList.js
+++ VirtualDom/todo/src/TodoList.js
@@ -5,7 +5,12 @@ const TodoList = (props) => {
     const todos = props.todos.map((todo, index) => {
         return <Todo key={index} title={todo} />
     });
-    return <ul>{todos}</ul>
+
+    if (todos.length == 0) {
+        return <h1>Add a Todo</h1>
+    } else {
+        return <ul>{todos}</ul>
+    }
 };
 
 export default TodoList;
